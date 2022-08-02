adf5b365e81d3579cf73b3458285dd7d79168569
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-02 03:37:19 +0000

    Add code to setup database connection to work with sqlalchemy

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql://darkbotbbl:testpass1234@localhost/myFastAPIDatabase"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=DATABASE_URL, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+# Dependency to get session connection to the database
+def get_db():
+    """Yield database session connection."""
+    db_session = SessionLocal()
+    try:
+        yield db_session
+    finally:
+        db_session.close()
