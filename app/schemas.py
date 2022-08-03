6bf942435c272716c7f50f12cc3a2330fb1e89c1
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-03 07:34:20 +0000

    Define different schemas for the Post pydantic model

---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    """Schema representation of a Post"""
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    """Schema representation for creating a post"""
+
+
+class PostUpdate(PostBase):
+    """Schema representation for updating a post"""
+    published: bool
