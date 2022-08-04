d9a674bf9f07a31e61efd9b0722bcd7b3b75aff6
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-04 03:18:48 +0000

    Add utility function that takes user's raw password and checks against database stored hashed password

--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,13 @@ ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(raw_password: str):
+    """Take a raw password and return a hashed password"""
     return ctx.hash(raw_password)
+
+
+def verify_password(raw_password: str, hashed_password):
+    """
+    Compare user's raw password to hashed_password
+    and return True if they match
+    """
+    return ctx.verify(raw_password, hashed_password)
