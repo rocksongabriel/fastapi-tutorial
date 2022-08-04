2d4014244c93a2c4cd18c6fdd5fe01c1a715a698
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-04 19:44:00 +0000

    Change from datetime.now() to datetime.utcnow()

--- app/oauth2.py
+++ app/oauth2.py
@@ -16,7 +16,9 @@ def create_access_token(data: dict) -> str:
     """Take data and encode it into a jwt token."""
 
     data_to_encode = data.copy()
-    expiration_time = datetime.now() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    expiration_time = datetime.utcnow() + timedelta(
+        minutes=TOKEN_EXPIRE_MINUTES
+    )
     data_to_encode.update({"exp": str(expiration_time)})
     access_token = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
@@ -46,7 +48,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid credentials or non existent account"
+        detail="Invalid credentials or non existent account",
     )
 
     return verify_access_token(token, credentials_exception)
