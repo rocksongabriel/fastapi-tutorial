e4f6b7e61c34d3cde9acb7a3fd806b995dff704b
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-04 03:19:05 +0000

    Register the router of the auth in the main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from app.routers import posts, users
+from app.routers import posts, users, auth
 
 
 # Create the database tables
@@ -13,6 +13,7 @@ app = FastAPI()
 
 app.include_router(posts.router)
 app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
